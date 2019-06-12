d95f0b9daffda1a85188227eb68889957f7dc68b
박시우 <sw.talk@nhnent.com>
2019-06-12 14:30:52 +0900

    Vue TodoList 작성

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <h1> Todo it! </h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped>
+h1 {
+  color: #4f3b52;
+  font-weight: 900;
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
